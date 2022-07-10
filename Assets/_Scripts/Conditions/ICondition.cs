97f17ac7a4a5281d01efc43b53571f0358e82c7e
Gökay <ygokayo@hotmail.com>
2022-07-10 21:35:10 +0300

    removed conditions & updated read-me

--- Assets/_Scripts/Conditions/ICondition.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//[CreateAssetMenu(menuName = "ScriptableObjects/Condition")]
-public interface ICondition
-{
-    public bool IsConditionMet();
-}
--- Assets/_Utilities/Editor/EditorReadMe.cs
+++ Assets/_Utilities/Editor/EditorReadMe.cs
@@ -29,7 +29,7 @@ public class EditorReadMeWindow : EditorWindow
 {
     private void OnEnable()
     {
-        position = new Rect(300, 200, 600, 400);
+        position = new Rect(300, 200, 1200, 400);
     }
 
     private void OnDisable()
@@ -39,12 +39,21 @@ public class EditorReadMeWindow : EditorWindow
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 600, 150), 
-           "Most Settings Can be configured from the 'Level Config' scriptable object in Resources.\n" +
-           "The Level Config SO should be assigned to the GameManager in the scene." +
+        GUI.Label(new Rect(10, 10, 1200, 150),
+           "Most Settings Can be configured from the 'Level Config' scriptable object in Resources and MANAGERS/GameManager in the scene." +
+           "\n" +
+           "\nI have developed the game as a pluggable behaviour system which is easy to expand upon,"+
+           "\nbecause most tower defense games have roguelite elements and tower upgrading these days." +
+           "\nWhile developing this project i had 5 days which 3 of them was work days so i couldn't have much time to spare" +
+           "\nDidn't have time to implement a saving system and dividing to project to assembly definitions yet i do have work experience on those cases as well."+
+           "\n" +
+           "\nScenes are saved with an editor script which can be accesed from Tools/Create Scene Pool" +
+           "\n" +
+           "\nHigh Score save data can be deleted from Tools/Delete Save Datas." +
            "\n" +
            "\nMore configurations can be found in the following scriptable objects:" +
-           "\n-> GridEntityDefinitions" +
-           "\n-> Conditions");
+           "\n-> Resources/ScriptableObjects/Behaviours" +
+           "\n-> Resources/ScriptableObjects/ScenePools"+
+           "\n-> Resources/ScriptableObjects/Pools");
     }
 }
